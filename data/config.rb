43a4dd3b14e944c8bef436c4088fb8777cefe52f
Vitali <madgamer@Hot.ee>
2012-06-12 18:16:06 +0300

    deleted

--- data/config.rb
+++
@@ -1,8 +0,0 @@
-ActiveRecord::Base.establish_connection(
-    :adapter => "mysql2",
-    :database => "test",
-    :username => "root",
-    :encoding => 'utf8',
-    :password => '12345',
-    :pool => 10
-  )
